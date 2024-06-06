3ea1ccb5290e95ba3c0054eea2c6611b01df053a
Wei Yang <weiyang@mailfence.com>
2024-06-06 19:16:27 +0200

    change description of lightning spell weapon

--- js/item/lightning_spell.js
+++ js/item/lightning_spell.js
@@ -4,7 +4,7 @@ class LightningSpell extends Item {
   constructor() {
     super();
     this.name = "Lightning Spell";
-    this.description = "A powerfull spell that deals damage to enemy in a radius";
+    this.description = "Creates a damaging aura";
     this.icon = "content/item_icons/staff/staff_03c.png";
     this.iconName = "lightningSpell";
   }
